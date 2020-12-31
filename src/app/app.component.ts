16cc9ecafcf9985f497442adb40745ac39a886b3
Lance <LanceHuang@ponddy-edu.com>
2020-12-31 10:54:17 +0800

    add platformId

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'hsk-landing';
+  platform: string;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+  }
+
+  public ngOnInit(): void {
+    this.platform = isPlatformBrowser(this.platformId) ? 'Browser' : 'Server';
+    console.log(this.platform)
+  }
 }
