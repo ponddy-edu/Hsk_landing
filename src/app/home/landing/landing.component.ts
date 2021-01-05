ec203543ece92aa86911333a750c7eb532a42bd1
Lance <LanceHuang@ponddy-edu.com>
2021-01-05 15:22:47 +0800

    add homa page landing mobile style

--- src/app/home/landing/landing.component.ts
+++ src/app/home/landing/landing.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DeviceService} from "../../../utils/device.service";
+
+const imageList = ['https://hsk-landing.s3-us-west-2.amazonaws.com/home/index_pic_01.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/index_pic_02.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/index_pic_03.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/index_pic_04.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/index_pic_05.png',
+]
+const imagmeListMobile = ['https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/index_pic_01_phone.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/index_pic_02_phone.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/index_pic_03_phone.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/index_pic_04_phone.png',
+  'https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/index_pic_05_phone.png',
+]
 
 @Component({
   selector: 'app-home-landing',
@@ -6,8 +20,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  carouselImageList: any[]
 
-  constructor() { }
+
+  constructor(private device: DeviceService) {
+    console.log('landing')
+    console.log(this.device.mobile)
+    if (this.device.mobile.getValue()) {
+      this.carouselImageList = imagmeListMobile
+    } else {
+      this.carouselImageList = imageList
+    }
+
+  }
 
   ngOnInit(): void {
   }
