60125f23c81a2a550fa9b8c2cfe5fd2f1ff95ac3
lambdaTW <lambda@lambda.tw>
2021-01-05 08:30:31 +0800

    Add deploy scripts

---
+++ deploy/deploy.template.sh
@@ -0,0 +1,26 @@
+export GIT_PROJECT_NAME=${CIRCLE_PROJECT_REPONAME}
+cd /projects/$GIT_PROJECT_NAME
+git pull -f
+
+# Env
+export CNAME=$(cat deploy/CNAME)
+export PROJECT_NAME=$(jq -r '.name' < package.json)
+# Rmove old docker image tag
+docker tag $PROJECT_NAME:latest $PROJECT_NAME:old && docker rmi $PROJECT_NAME:latest || echo "no old image"
+
+# Load docker image
+docker load < /images/$PROJECT_NAME.tar
+# Copy nginx file
+envsubst '${PROJECT_NAME},${CNAME}' < deploy/nginx.template.conf > /composes/enabled_sites/${PROJECT_NAME}.conf
+# Copy docker-compose file
+envsubst '${PROJECT_NAME}' < deploy/docker-compose.yml > /composes/${PROJECT_NAME}.yml
+
+# Run docker compose
+cd /composes
+echo -n "" > files.args
+ls -1 | grep .yml | grep -v \~ | while read f
+do
+    echo -n " -f $f" >> files.args
+done
+docker-compose $(cat files.args) down
+docker-compose $(cat files.args) up -d
