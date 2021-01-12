ab1a3ac4d572aed35b098756390b75fb3a4c442d
Lance <LanceHuang@ponddy-edu.com>
2021-01-12 17:47:14 +0800

    fix style of pad

--- src/app/ponddy/about-ponddy/about-ponddy.component.ts
+++ src/app/ponddy/about-ponddy/about-ponddy.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {DeviceService} from '../../../utils/device.service';
-import {CarouselComponent} from "angular-responsive-carousel";
+import {CarouselComponent} from 'angular-responsive-carousel';
 
 const imageList = ['https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/logo_1.png',
   'https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/logo_2.png',
