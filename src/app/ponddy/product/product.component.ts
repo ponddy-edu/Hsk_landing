ecb257b1b4017800bdf9aa1049e2da03b5a61b41
Lance <LanceHuang@ponddy-edu.com>
2021-01-13 14:23:53 +0800

    add Responsive images at product of ponddy

--- src/app/ponddy/product/product.component.ts
+++ src/app/ponddy/product/product.component.ts
@@ -12,10 +12,10 @@ export class ProductComponent implements OnInit, OnDestroy {
   widthSubscribe: Subscription
   tutorsPic = `https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/mobile/tutors_pic_phone.png 767w,
   https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/tutors_pic_web.png 1100w`
-  readerPic = `https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/mobile/reader_pic_web.png 767w,
+  readerPic = `https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/mobile/reader_pic_phone.png 767w,
   https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/reader_pic_web.png 1100w`
   smartTextbookPic = `https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/smartTextbook_pic_web.png 1100w,
-            https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/mobile/smartTextbook_pic_web.png 767w`
+            https://hsk-landing.s3-us-west-2.amazonaws.com/ponddy/mobile/smartTextbook_pic_phone.png 767w`
   constructor(public device: DeviceService) {
     this.widthSubscribe = device.$screenWidth
       .subscribe(width => this.screenWidth = width)
