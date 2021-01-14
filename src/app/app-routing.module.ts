be19d165fe0f725f7a58a3137c7a8eb748805e65
Lance <LanceHuang@ponddy-edu.com>
2021-01-14 16:59:42 +0800

    add test router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {HomeModule} from './home/home.module';
-import {PonddyModule} from "./ponddy/ponddy.module";
+import {PonddyModule} from './ponddy/ponddy.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', loadChildren: './home/home.module#HomeModule'},
-  {path: 'ponddy', loadChildren: './ponddy/ponddy.module#PonddyModule'}
+  {path: 'ponddy', loadChildren: './ponddy/ponddy.module#PonddyModule'},
+  {path: 'test', loadChildren: './test/test.module#TestModule'}
+
   // { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
