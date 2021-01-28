f1ed4217198cdbac1c7afa5e71b851c94a127511
Tina <>
2021-01-28 17:13:51 +0800

    add select section for faq

--- src/app/faq/faq.module.ts
+++ src/app/faq/faq.module.ts
@@ -4,12 +4,13 @@ import { FaqComponent } from './faq.component';
 import {RouterModule, Routes} from '@angular/router';
 import {ShareModule} from '../../components/share.module';
 import {LandingComponent} from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path: 'faq', component: FaqComponent},
 ];
 @NgModule({
-  declarations: [FaqComponent, LandingComponent],
+  declarations: [FaqComponent, LandingComponent, AboutComponent],
   imports: [
     CommonModule,
     ShareModule,
