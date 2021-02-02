4f03964942db12c27e99f651776d7b0cbb48a3ca
Tina <>
2021-02-02 11:55:42 +0800

    update tab style section for faq mobile

--- src/app/faq/about/about.component.ts
+++ src/app/faq/about/about.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  form01 = `https://hsk-landing.s3-us-west-2.amazonaws.com/faq/mobile/form_01_phone.svg 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/faq/web/form_01_web.svg 1100w`
+  form02 = `https://hsk-landing.s3-us-west-2.amazonaws.com/faq/mobile/form_02_phone.svg 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/faq/web/form_02_web.svg 1100w`
+
 
   constructor() { }
 
