da0a497a3583c3586cc349b375f5637dfb4f9262
Tina <>
2021-02-04 14:44:27 +0800

    add smart section

--- src/app/resources/intro-smart/intro-smart.component.ts
+++ src/app/resources/intro-smart/intro-smart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DeviceService} from '../../../utils/device.service';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-intro-smart',
@@ -8,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class IntroSmartComponent implements OnInit {
   picDailyWord = `https://hsk-landing.s3-us-west-2.amazonaws.com/resources/smart/mobile/pic_dailyWord_phone.png 767w,
   https://hsk-landing.s3-us-west-2.amazonaws.com/resources/smart/web/pic_dailyWord.png 1100w`
+  picDigestWord = `https://hsk-landing.s3-us-west-2.amazonaws.com/resources/smart/mobile/pic_digest_phone.png 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/resources/smart/web/pic_digest.png 1100w`
 
-  constructor() { }
 
-  ngOnInit(): void {
+  mobile = true
+  web = false
+
+  screenWidth = 900
+  widthSubscribe: Subscription
+  constructor(public device: DeviceService) {
+    this.widthSubscribe = device.$screenWidth
+      .subscribe(width => this.screenWidth = width)
+
+      if (device.$mobile.getValue()) {
+        this.mobile = true
+        this.web = false
+      } else {
+        this.mobile = false
+        this.web = true
+      }
   }
 
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.widthSubscribe.unsubscribe()
+  }
 }
