30620485ec063c30d77cdedf5f56b84b09a878f9
Tina <>
2021-02-04 16:37:34 +0800

    update ondestroy

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,17 +1,18 @@
 import {collectExternalReferences} from '@angular/compiler';
-import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {BehaviorSubject} from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.scss']
 })
-export class ResourcesComponent implements OnInit {
+export class ResourcesComponent implements OnInit, OnDestroy{
   public tab = 0
   isDownloadPage = false
-
+  routeSubscribe:Subscription
   constructor(private activatedRoute: ActivatedRoute) {
 
   }
@@ -19,15 +20,19 @@ export class ResourcesComponent implements OnInit {
 
   ngOnInit(): void {
     // this.activatedRoute.data.subscribe(res => console.log(res))
-    this.activatedRoute.url.subscribe((res) => {
+    this.routeSubscribe = this.activatedRoute.url.
+    subscribe((res) => {
       const tabIndex = this.activatedRoute.snapshot.firstChild ? this.activatedRoute.snapshot.firstChild.data.tab : 0;
-      console.log(tabIndex)
       if (tabIndex < 3) {
         this.setTab(tabIndex)
       } else if (tabIndex === 3) {
         this.setDownloadRe(true)
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscribe.unsubscribe()
   }
 
   setTab(tabIndex: number) {
