749ce16c60684e6d08757c1410cfeee3e834eb99
Lance <LanceHuang@ponddy-edu.com>
2021-02-24 17:03:03 +0800

    feat: add target mobile carouse

--- src/app/home/target/target.component.ts
+++ src/app/home/target/target.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DeviceService} from '../../../utils/device.service';
 
 @Component({
   selector: 'app-target',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./target.component.scss']
 })
 export class TargetComponent implements OnInit {
+  picStudySmart = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_StudySmart.png 1100w,
+             https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_StudySmart_P.png 767w`
 
-  constructor() { }
+  picTest = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_TestYourAbility.png 1100w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_TestYourAbility_P.png 767w`
+
+  picPractice = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_PracticeWithEase.png 1100w,
+      https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_PracticeWithEase_P.png 767w`
+
+  mobile = true
+  constructor(public device: DeviceService) {
+    if (device.$mobile.getValue()) {
+      this.mobile = true
+    } else {
+      this.mobile = false
+    }
+  }
 
   ngOnInit(): void {
   }
