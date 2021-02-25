d71acfec80b97b30a2dc94b291ff8f1c1618e698
Lance <LanceHuang@ponddy-edu.com>
2021-02-25 09:30:24 +0800

    fix: update picList at mobile

--- src/app/home/target/target.component.ts
+++ src/app/home/target/target.component.ts
@@ -7,14 +7,14 @@ import {DeviceService} from '../../../utils/device.service';
   styleUrls: ['./target.component.scss']
 })
 export class TargetComponent implements OnInit {
-  picStudySmart = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_StudySmart.png 1100w,
-             https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_StudySmart_P.png 767w`
+  picStudySmart = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_StudySmart_P.png 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_StudySmart.png 1100w`
 
-  picTest = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_TestYourAbility.png 1100w,
-  https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_TestYourAbility_P.png 767w`
+  picTest = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_TestYourAbility_P.png 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_TestYourAbility.png 1100w`
 
-  picPractice = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_PracticeWithEase.png 1100w,
-      https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_PracticeWithEase_P.png 767w`
+  picPractice = `https://hsk-landing.s3-us-west-2.amazonaws.com/home/mobile/pic_PracticeWithEase_P.png 767w,
+  https://hsk-landing.s3-us-west-2.amazonaws.com/home/target_PracticeWithEase.png 1100w,`
 
   mobile = true
   constructor(public device: DeviceService) {
