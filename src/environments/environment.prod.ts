5561104713241998431f166290f210155b2cf517
Tina <>
2021-03-04 11:13:57 +0800

    update url on production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   appUrl: 'http://hsk.ponddy.com/',
   clientId: "fe020207-6e96-4c58-963fa34321042c26",
-  authApiUrl: "https://auth-dev.ponddy.com",
+  authApiUrl: "https://auth.ponddy.com",
   apiUrl: "auth.ponddy.com/api",
   redirectUri: "https://reader.ponddy.com/app/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
+  production: true,
   appUrl: 'http://hsk.ponddy.com/',
   clientId: "fe020207-6e96-4c58-963fa34321042c26",
-  authApiUrl: "https://auth-dev.ponddy.com",
-  apiUrl: "https://auth-dev.ponddy.com/api",
-  redirectUri: "https://reader-dev.ponddy.com/app/",
+  authApiUrl: "https://auth.ponddy.com",
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
