1e6d1a7f878483552726849558ba8690ce72570e
Lance <LanceHuang@ponddy-edu.com>
2021-03-10 10:35:22 +0800

    fix: add stripeKey at environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
   production: true,
   appUrl: 'https://hsk.ponddy.com',
-  clientId: "b4df9839-3872-47b1-a4dc-01f30f981df9",
-  authApiUrl: "https://auth.ponddy.com",
-  apiUrl: "auth.ponddy.com/api",
-  redirectUri: "https://reader.ponddy.com/app/",
+  clientId: 'b4df9839-3872-47b1-a4dc-01f30f981df9',
+  authApiUrl: 'https://auth.ponddy.com',
+  apiUrl: 'auth.ponddy.com/api',
+  redirectUri: 'https://reader.ponddy.com/app/',
+  stripeKey: 'pk_live_Pz4r4nqTVPSZC1Puk1lOBL0l',
+  stripe_product_enroll_test: 'price_1IQomRHRhoOpWeKwWHN5nFdy'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
   appUrl: 'https://hsk.ponddy.com',
-  clientId: "b4df9839-3872-47b1-a4dc-01f30f981df9",
-  authApiUrl: "https://auth.ponddy.com",
-  GAPI_CLIENT_ID: '216992628116-3c5j7d6qeufau4mvr7tp9m15bce28nvf.apps.googleusercontent.com'
+  clientId: 'b4df9839-3872-47b1-a4dc-01f30f981df9',
+  authApiUrl: 'https://auth.ponddy.com',
+  GAPI_CLIENT_ID: '216992628116-3c5j7d6qeufau4mvr7tp9m15bce28nvf.apps.googleusercontent.com',
+  stripeKey: 'pk_test_nfaV6dM7kUaIqtYxBt4cOjFe',
+  stripe_product_enroll_test: 'price_1IQoidHRhoOpWeKwa2EUDldx'
 };
