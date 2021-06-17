6bf2a172c3694c8d9244a4363f3a6468710a560b
Tina <>
2021-06-18 00:04:35 +0800

    fix(courses): update Group Class content

--- src/app/courses/intro-course/intro-course.component.ts
+++ src/app/courses/intro-course/intro-course.component.ts
@@ -7,8 +7,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./intro-course.component.scss']
 })
 export class IntroCourseComponent implements OnInit {
+  mobile = true
+  web = false
+  courseWidth: number
+  courseHeight: number
 
   constructor(public device: DeviceService) {
+    if (device.$mobile.getValue()) {
+      this.courseWidth = 240
+      this.courseHeight = 550
+      this.mobile = true
+      this.web = false
+    } else {
+      this.courseHeight = 600
+      this.courseWidth = 500
+      this.mobile = false
+      this.web = true
+    }
   }
 
   pic_HSK12 = {
