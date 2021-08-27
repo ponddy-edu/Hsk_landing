ca82f4745d89bd7636fc353492bdf9abfba8ae54
LanceHuang <lancehuang@ponddy-edu.com>
2021-08-27 11:08:05 +0800

    feat(enroll test): add photo preview for photo upload

---
+++ src/app/enroll-test/step/selectList.ts
@@ -0,0 +1,1255 @@
+export abstract class SlectList {
+  public static motherTongueList = [
+    {
+      key: 101,
+      value: 'Afrikaans'
+    },
+    {
+      key: 405,
+      value: 'Albanian'
+    },
+    {
+      key: 107,
+      value: 'Amharic'
+    },
+    {
+      key: 501,
+      value: 'Arabic'
+    },
+    {
+      key: 401,
+      value: 'Armenian'
+    },
+    {
+      key: 408,
+      value: 'Belarussian'
+    },
+    {
+      key: 305,
+      value: 'Bengali'
+    },
+    {
+      key: 407,
+      value: 'Bulgarian'
+    },
+    {
+      key: 307,
+      value: 'Burmese'
+    },
+    {
+      key: 410,
+      value: 'Catalan (Provencal)'
+    },
+    {
+      key: 315,
+      value: 'Chinese'
+    },
+    {
+      key: 413,
+      value: 'Czech'
+    },
+    {
+      key: 416,
+      value: 'Danish'
+    },
+    {
+      key: 419,
+      value: 'Dutch'
+    },
+    {
+      key: 422,
+      value: 'English'
+    },
+    {
+      key: 425,
+      value: 'Estonian'
+    },
+    {
+      key: 504,
+      value: 'Farsi (Persian)'
+    },
+    {
+      key: 428,
+      value: 'Finnish'
+    },
+    {
+      key: 434,
+      value: 'French'
+    },
+    {
+      key: 437,
+      value: 'German'
+    },
+    {
+      key: 438,
+      value: 'Georgian'
+    },
+    {
+      key: 440,
+      value: 'Greek'
+    },
+    {
+      key: 201,
+      value: 'Guarani'
+    },
+    {
+      key: 507,
+      value: 'Hebrew'
+    },
+    {
+      key: 323,
+      value: 'Hindi'
+    },
+    {
+      key: 443,
+      value: 'Hungarian (Magyar)'
+    },
+    {
+      key: 447,
+      value: 'Icelandic'
+    },
+    {
+      key: 328,
+      value: 'Indonesian'
+    },
+    {
+      key: 450,
+      value: 'Italian'
+    },
+    {
+      key: 331,
+      value: 'Japanese'
+    },
+    {
+      key: 339,
+      value: 'Kazakh'
+    },
+    {
+      key: 310,
+      value: 'Khmer (Kampuchean)'
+    },
+    {
+      key: 340,
+      value: 'Korean'
+    },
+    {
+      key: 342,
+      value: 'Kurdish'
+    },
+    {
+      key: 343,
+      value: 'Lao'
+    },
+    {
+      key: 452,
+      value: 'Latvian'
+    },
+    {
+      key: 453,
+      value: 'Lithuanian'
+    },
+    {
+      key: 454,
+      value: 'Macedonian'
+    },
+    {
+      key: 345,
+      value: 'Malay'
+    },
+    {
+      key: 455,
+      value: 'Maltese'
+    },
+    {
+      key: 607,
+      value: 'Marshallese'
+    },
+    {
+      key: 341,
+      value: 'Mongolian'
+    },
+    {
+      key: 351,
+      value: 'Nepali'
+    },
+    {
+      key: 456,
+      value: 'Norwegian'
+    },
+    {
+      key: 357,
+      value: 'Pashto'
+    },
+    {
+      key: 459,
+      value: 'Polish'
+    },
+    {
+      key: 461,
+      value: 'Portuguese'
+    },
+    {
+      key: 464,
+      value: 'Romanian'
+    },
+    {
+      key: 141,
+      value: 'Ruanda'
+    },
+    {
+      key: 467,
+      value: 'Russian'
+    },
+    {
+      key: 365,
+      value: 'Samar-Leyte'
+    },
+    {
+      key: 616,
+      value: 'Samoan'
+    },
+    {
+      key: 470,
+      value: 'Serbo-Croatian'
+    },
+    {
+      key: 361,
+      value: 'Sinhalese'
+    },
+    {
+      key: 473,
+      value: 'Slovak'
+    },
+    {
+      key: 474,
+      value: 'Slovene'
+    },
+    {
+      key: 173,
+      value: 'Somali'
+    },
+    {
+      key: 478,
+      value: 'Spanish'
+    },
+    {
+      key: 176,
+      value: 'Swahili'
+    },
+    {
+      key: 481,
+      value: 'Swedish'
+    },
+    {
+      key: 370,
+      value: 'Tamil'
+    },
+    {
+      key: 376,
+      value: 'Thai'
+    },
+    {
+      key: 622,
+      value: 'Tongan'
+    },
+    {
+      key: 484,
+      value: 'Turkish'
+    },
+    {
+      key: 483,
+      value: 'Turkmen'
+    },
+    {
+      key: 487,
+      value: 'Ukrainian'
+    },
+    {
+      key: 385,
+      value: 'Urdu'
+    },
+    {
+      key: 495,
+      value: 'Uzbek'
+    },
+    {
+      key: 388,
+      value: 'Vietnamese'
+    },
+    {
+      key: 999,
+      value: 'Other'
+    }
+  ]
+  public static nationalityList = [
+    {
+      key: 502,
+      value: 'AFGHANISTAN'
+    },
+    {
+      key: 601,
+      value: 'ALBANIA'
+    },
+    {
+      key: 701,
+      value: 'ALGERIA'
+    },
+    {
+      key: 603,
+      value: 'ANDORRA'
+    },
+    {
+      key: 704,
+      value: 'ANGOIA'
+    },
+    {
+      key: 901,
+      value: 'ANGUILLA'
+    },
+    {
+      key: 902,
+      value: 'ANTIGUA'
+    },
+    {
+      key: 937,
+      value: 'ARGENTINA'
+    },
+    {
+      key: 640,
+      value: 'ARMENIA'
+    },
+    {
+      key: 950,
+      value: 'Aruba'
+    },
+    {
+      key: 801,
+      value: 'AUSTRALIA'
+    },
+    {
+      key: 604,
+      value: 'AUSTRIA'
+    },
+    {
+      key: 639,
+      value: 'AZERBAIJAN'
+    },
+    {
+      key: 508,
+      value: 'BAHRAIN'
+    },
+    {
+      key: 521,
+      value: 'BANGLADESH'
+    },
+    {
+      key: 903,
+      value: 'BARBADOS'
+    },
+    {
+      key: 637,
+      value: 'BELARUS'
+    },
+    {
+      key: 606,
+      value: 'BELGIUM'
+    },
+    {
+      key: 908,
+      value: 'BELIZE'
+    },
+    {
+      key: 705,
+      value: 'BENIN'
+    },
+    {
+      key: 906,
+      value: 'BERMUDA'
+    },
+    {
+      key: 509,
+      value: 'BHUTAN'
+    },
+    {
+      key: 941,
+      value: 'BOLIVIA'
+    },
+    {
+      key: 648,
+      value: 'BOSNIA AND HERZEGOVINA'
+    },
+    {
+      key: 706,
+      value: 'BOTSWANA'
+    },
+    {
+      key: 825,
+      value: 'Bouvet Island'
+    },
+    {
+      key: 939,
+      value: 'BRAZIL'
+    },
+    {
+      key: 936,
+      value: 'BRITISHI VIRGIN ISLANDS'
+    },
+    {
+      key: 531,
+      value: 'BRUNEI'
+    },
+    {
+      key: 605,
+      value: 'BULGARIA'
+    },
+    {
+      key: 741,
+      value: 'BURKINA FASO'
+    },
+    {
+      key: 707,
+      value: 'BURUNDI'
+    },
+    {
+      key: 513,
+      value: 'CAMBODIA'
+    },
+    {
+      key: 719,
+      value: 'CAMEROON'
+    },
+    {
+      key: 919,
+      value: 'CANADA'
+    },
+    {
+      key: 954,
+      value: 'Canary Islands'
+    },
+    {
+      key: 710,
+      value: 'CAPE VERDE'
+    },
+    {
+      key: 920,
+      value: 'CAYMAN ISLANDS'
+    },
+    {
+      key: 755,
+      value: 'CENTRAL AFRICA'
+    },
+    {
+      key: 753,
+      value: 'CHAD'
+    },
+    {
+      key: 949,
+      value: 'CHILE'
+    },
+    {
+      key: 501,
+      value: 'CHINA'
+    },
+    {
+      key: 830,
+      value: 'Christmas Island'
+    },
+    {
+      key: 828,
+      value: 'Cocos(Keeling) Islands'
+    },
+    {
+      key: 944,
+      value: 'COLOMBIA'
+    },
+    {
+      key: 910,
+      value: 'Commonwealth of Dominica'
+    },
+    {
+      key: 824,
+      value: 'Commonwealth of the Northern Marianas'
+    },
+    {
+      key: 913,
+      value: 'COSTA RICA'
+    },
+    {
+      key: 751,
+      value: 'COTE D’IVOIRE'
+    },
+    {
+      key: 645,
+      value: 'CROATIA'
+    },
+    {
+      key: 914,
+      value: 'CUBA'
+    },
+    {
+      key: 526,
+      value: 'CYPRUS'
+    },
+    {
+      key: 615,
+      value: 'CZECH'
+    },
+    {
+      key: 510,
+      value: 'DEMOCRATIC P.R. KOREA'
+    },
+    {
+      key: 609,
+      value: 'DENMARK'
+    },
+    {
+      key: 713,
+      value: 'DJIBOUTI'
+    },
+    {
+      key: 511,
+      value: 'EAST TIMOR'
+    },
+    {
+      key: 942,
+      value: 'ECUADOR'
+    },
+    {
+      key: 702,
+      value: 'EGYPT'
+    },
+    {
+      key: 927,
+      value: 'EL SALVADOR'
+    },
+    {
+      key: 708,
+      value: 'EQIATORIAL GUINCA'
+    },
+    {
+      key: 758,
+      value: 'ERITREA'
+    },
+    {
+      key: 641,
+      value: 'ESTONIA'
+    },
+    {
+      key: 703,
+      value: 'ETHIOPIA'
+    },
+    {
+      key: 951,
+      value: 'Falkland Islands(Malvinas)'
+    },
+    {
+      key: 805,
+      value: 'FIJI'
+    },
+    {
+      key: 613,
+      value: 'FINLAND'
+    },
+    {
+      key: 610,
+      value: 'FRANCE'
+    },
+    {
+      key: 943,
+      value: 'FRENCH GUIANA'
+    },
+    {
+      key: 804,
+      value: 'FRENCH POLYNESIA'
+    },
+    {
+      key: 827,
+      value: 'FRENCH SOUTHERN TERRITORIES'
+    },
+    {
+      key: 717,
+      value: 'GABON'
+    },
+    {
+      key: 644,
+      value: 'GEORGIA'
+    },
+    {
+      key: 616,
+      value: 'GERMANY'
+    },
+    {
+      key: 716,
+      value: 'GHANA'
+    },
+    {
+      key: 653,
+      value: 'Gibraltar'
+    },
+    {
+      key: 631,
+      value: 'GREECE'
+    },
+    {
+      key: 912,
+      value: 'GREENLAND'
+    },
+    {
+      key: 911,
+      value: 'GRENADA'
+    },
+    {
+      key: 915,
+      value: 'GUADELOUPE'
+    },
+    {
+      key: 806,
+      value: 'GUAM'
+    },
+    {
+      key: 934,
+      value: 'GUATEMALA'
+    },
+    {
+      key: 714,
+      value: 'GUINEA'
+    },
+    {
+      key: 715,
+      value: 'GUINEA-BISSAU'
+    },
+    {
+      key: 945,
+      value: 'GUYANA'
+    },
+    {
+      key: 916,
+      value: 'HAITI'
+    },
+    {
+      key: 826,
+      value: 'Heard islands and Mc Donald Islands'
+    },
+    {
+      key: 918,
+      value: 'HONDURAS'
+    },
+    {
+      key: 543,
+      value: 'HONGKONG'
+    },
+    {
+      key: 632,
+      value: 'HUNGARY'
+    },
+    {
+      key: 607,
+      value: 'ICELAND'
+    },
+    {
+      key: 819,
+      value: 'Independent State of Samoa'
+    },
+    {
+      key: 539,
+      value: 'INDIA'
+    },
+    {
+      key: 540,
+      value: 'INDONESIA'
+    },
+    {
+      key: 538,
+      value: 'IRAN'
+    },
+    {
+      key: 536,
+      value: 'IRAQ'
+    },
+    {
+      key: 602,
+      value: 'IRELAND'
+    },
+    {
+      key: 537,
+      value: 'ISRAEL'
+    },
+    {
+      key: 633,
+      value: 'ITALY'
+    },
+    {
+      key: 935,
+      value: 'JAMAICA'
+    },
+    {
+      key: 525,
+      value: 'JAPAN'
+    },
+    {
+      key: 541,
+      value: 'JORDAN'
+    },
+    {
+      key: 547,
+      value: 'KAZAKHSTAN'
+    },
+    {
+      key: 721,
+      value: 'KENYA'
+    },
+    {
+      key: 617,
+      value: 'KIECHTENSTEIN'
+    },
+    {
+      key: 807,
+      value: 'KIRIBATI'
+    },
+    {
+      key: 515,
+      value: 'KUWAIT'
+    },
+    {
+      key: 546,
+      value: 'KYRGHYZSTAN'
+    },
+    {
+      key: 516,
+      value: 'LAOS'
+    },
+    {
+      key: 642,
+      value: 'LATVIA'
+    },
+    {
+      key: 517,
+      value: 'LEBANON'
+    },
+    {
+      key: 722,
+      value: 'LESOTHO'
+    },
+    {
+      key: 723,
+      value: 'LIBERIA'
+    },
+    {
+      key: 724,
+      value: 'LIBYA'
+    },
+    {
+      key: 643,
+      value: 'LITHUANIA'
+    },
+    {
+      key: 618,
+      value: 'LUXEMBOURG'
+    },
+    {
+      key: 544,
+      value: 'MACAO'
+    },
+    {
+      key: 649,
+      value: 'MACEDONIA'
+    },
+    {
+      key: 727,
+      value: 'MADAGASCAR'
+    },
+    {
+      key: 955,
+      value: 'madeira'
+    },
+    {
+      key: 728,
+      value: 'MALAWI'
+    },
+    {
+      key: 519,
+      value: 'MALAYSIA'
+    },
+    {
+      key: 518,
+      value: 'MALDIVES'
+    },
+    {
+      key: 729,
+      value: 'MALI'
+    },
+    {
+      key: 620,
+      value: 'MALTA'
+    },
+    {
+      key: 822,
+      value: 'MARSHALL ISLANDS'
+    },
+    {
+      key: 921,
+      value: 'MARTINIQUE'
+    },
+    {
+      key: 731,
+      value: 'MAURITANIA'
+    },
+    {
+      key: 730,
+      value: 'MAURITIUS'
+    },
+    {
+      key: 756,
+      value: 'Mayotte'
+    },
+    {
+      key: 925,
+      value: 'MEXICO'
+    },
+    {
+      key: 823,
+      value: 'MICRONESIA'
+    },
+    {
+      key: 638,
+      value: 'MOLDOVA'
+    },
+    {
+      key: 622,
+      value: 'MONACO'
+    },
+    {
+      key: 520,
+      value: 'MONGOLIA'
+    },
+    {
+      key: 651,
+      value: 'Montenegro'
+    },
+    {
+      key: 924,
+      value: 'MONTSERRAT'
+    },
+    {
+      key: 732,
+      value: 'MOROCCO'
+    },
+    {
+      key: 733,
+      value: 'MOZAMBIQUE'
+    },
+    {
+      key: 522,
+      value: 'MYANMAR'
+    },
+    {
+      key: 734,
+      value: 'NAMDIA'
+    },
+    {
+      key: 809,
+      value: 'NAURU'
+    },
+    {
+      key: 524,
+      value: 'NEPAL'
+    },
+    {
+      key: 917,
+      value: 'NETHERLANDS ANTIKKES'
+    },
+    {
+      key: 820,
+      value: 'NEW CALEDONIA'
+    },
+    {
+      key: 821,
+      value: 'NEW ZEALAND'
+    },
+    {
+      key: 926,
+      value: 'NICAREGUA'
+    },
+    {
+      key: 735,
+      value: 'NIGER'
+    },
+    {
+      key: 736,
+      value: 'NIGERIA'
+    },
+    {
+      key: 810,
+      value: 'NIUE ISLAND'
+    },
+    {
+      key: 829,
+      value: 'Norfolk Island'
+    },
+    {
+      key: 624,
+      value: 'NORWAY'
+    },
+    {
+      key: 505,
+      value: 'OMAN'
+    },
+    {
+      key: 999,
+      value: 'Other'
+    },
+    {
+      key: 506,
+      value: 'PAKISTAN'
+    },
+    {
+      key: 813,
+      value: 'Palau'
+    },
+    {
+      key: 507,
+      value: 'PALESTINE'
+    },
+    {
+      key: 905,
+      value: 'PANAMA'
+    },
+    {
+      key: 802,
+      value: 'PAPUA NEW GUINEA'
+    },
+    {
+      key: 938,
+      value: 'PARAGUAY'
+    },
+    {
+      key: 940,
+      value: 'PERU'
+    },
+    {
+      key: 512,
+      value: 'PHILIPPINES'
+    },
+    {
+      key: 811,
+      value: 'PITCAIRN ISLAND'
+    },
+    {
+      key: 608,
+      value: 'POLAND'
+    },
+    {
+      key: 625,
+      value: 'PORTUGAL'
+    },
+    {
+      key: 907,
+      value: 'PUERTO RICO'
+    },
+    {
+      key: 514,
+      value: 'QATAR'
+    },
+    {
+      key: 757,
+      value: 'Republic of South Sudan'
+    },
+    {
+      key: 725,
+      value: 'REUNION'
+    },
+    {
+      key: 619,
+      value: 'ROMANIA'
+    },
+    {
+      key: 635,
+      value: 'RUSSIA'
+    },
+    {
+      key: 726,
+      value: 'RWANDA'
+    },
+    {
+      key: 928,
+      value: 'SAINT KITTS AND NEVIS'
+    },
+    {
+      key: 628,
+      value: 'SAN MARINO'
+    },
+    {
+      key: 742,
+      value: 'SAO TOME AND PRINCIP'
+    },
+    {
+      key: 956,
+      value: 'SATA Air Acores'
+    },
+    {
+      key: 527,
+      value: 'SAUDI ARABIA'
+    },
+    {
+      key: 739,
+      value: 'SENEGAL'
+    },
+    {
+      key: 650,
+      value: 'SERBIA'
+    },
+    {
+      key: 740,
+      value: 'SEYCHELLES'
+    },
+    {
+      key: 738,
+      value: 'SIERRA LEONE'
+    },
+    {
+      key: 533,
+      value: 'SINGAPORE'
+    },
+    {
+      key: 647,
+      value: 'SLOVAK'
+    },
+    {
+      key: 646,
+      value: 'SLOVENIA'
+    },
+    {
+      key: 812,
+      value: 'SOLOMON ISLANDS'
+    },
+    {
+      key: 746,
+      value: 'SOMALIA'
+    },
+    {
+      key: 737,
+      value: 'SOUTH AFRICA'
+    },
+    {
+      key: 953,
+      value: 'South Georgia and South Sandwich Islands'
+    },
+    {
+      key: 630,
+      value: 'SPAIN'
+    },
+    {
+      key: 528,
+      value: 'SRI LANKA'
+    },
+    {
+      key: 743,
+      value: 'ST.HELENA,ASCENSION'
+    },
+    {
+      key: 929,
+      value: 'ST.LUCIA'
+    },
+    {
+      key: 930,
+      value: 'ST.PIERRE AND MIQUEL'
+    },
+    {
+      key: 931,
+      value: 'ST.VINCENT'
+    },
+    {
+      key: 946,
+      value: 'SURINAM'
+    },
+    {
+      key: 652,
+      value: 'Svalbard and Jan mayen islands'
+    },
+    {
+      key: 744,
+      value: 'SWAZILAND'
+    },
+    {
+      key: 626,
+      value: 'SWEDEN'
+    },
+    {
+      key: 627,
+      value: 'SWITZERLAND'
+    },
+    {
+      key: 534,
+      value: 'SYRIA'
+    },
+    {
+      key: 548,
+      value: 'TADZHIKISTAN'
+    },
+    {
+      key: 550,
+      value: 'TAIWAN'
+    },
+    {
+      key: 747,
+      value: 'TANZANIA'
+    },
+    {
+      key: 529,
+      value: 'THAILAND'
+    },
+    {
+      key: 904,
+      value: 'THE BAHAMAS'
+    },
+    {
+      key: 720,
+      value: 'THE COMOROS'
+    },
+    {
+      key: 808,
+      value: 'THE COOK ISLANDS'
+    },
+    {
+      key: 754,
+      value: 'THE DEMOCRATIC REPUBLIC OF CONGO'
+    },
+    {
+      key: 909,
+      value: 'THE DOMINICAN REPUBLIC'
+    },
+    {
+      key: 611,
+      value: 'THE FAEROE ISLANDS'
+    },
+    {
+      key: 711,
+      value: 'THE GAMAIA'
+    },
+    {
+      key: 614,
+      value: 'THE NETHERLANDS'
+    },
+    {
+      key: 712,
+      value: 'THE REPUBLIC OF CONGO'
+    },
+    {
+      key: 523,
+      value: 'THE REPUBLIC OF KOREA'
+    },
+    {
+      key: 745,
+      value: 'THE SUDAN'
+    },
+    {
+      key: 709,
+      value: 'TOGO'
+    },
+    {
+      key: 816,
+      value: 'TOKELAU ISLANDS'
+    },
+    {
+      key: 814,
+      value: 'TONGA'
+    },
+    {
+      key: 933,
+      value: 'TRINIDAD AND TOBAGO'
+    },
+    {
+      key: 748,
+      value: 'TUNISIA'
+    },
+    {
+      key: 530,
+      value: 'TURKEY'
+    },
+    {
+      key: 549,
+      value: 'TURKMENISTAN'
+    },
+    {
+      key: 932,
+      value: 'TURKS AND CAICOS ISL'
+    },
+    {
+      key: 815,
+      value: 'TUYALU'
+    },
+    {
+      key: 922,
+      value: 'U.S.A'
+    },
+    {
+      key: 749,
+      value: 'UGANDA'
+    },
+    {
+      key: 636,
+      value: 'UKRAINE'
+    },
+    {
+      key: 503,
+      value: 'UNITED ARAB EMIRATES'
+    },
+    {
+      key: 634,
+      value: 'UNITED KINGDOM'
+    },
+    {
+      key: 952,
+      value: 'United States Minor Outlying Islands'
+    },
+    {
+      key: 948,
+      value: 'URUGUAY'
+    },
+    {
+      key: 545,
+      value: 'UZBEKISTAN'
+    },
+    {
+      key: 818,
+      value: 'VANUATU'
+    },
+    {
+      key: 612,
+      value: 'VATICAN'
+    },
+    {
+      key: 947,
+      value: 'VENEZUELA'
+    },
+    {
+      key: 542,
+      value: 'VIET NAM'
+    },
+    {
+      key: 923,
+      value: 'VIRGIN ISLANDS OF TH'
+    },
+    {
+      key: 817,
+      value: 'WALLIS AND FUTUNA'
+    },
+    {
+      key: 750,
+      value: 'WESTERN SAHARA'
+    },
+    {
+      key: 504,
+      value: 'YEMEN'
+    },
+    {
+      key: 752,
+      value: 'ZAMBIA'
+    },
+    {
+      key: 718,
+      value: 'ZIMBABWE'
+    }
+  ]
+
+}
