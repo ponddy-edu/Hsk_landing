d6451da9489baf5a93dc37308e7c878fe6149b06
tinachen <tinachen@ponddy-edu.com>
2022-04-28 14:55:06 +0800

    fix: update wrong screen position of home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {CarouselComponent} from 'angular-responsive-carousel';
 import {SheetService} from '../../utils/sheet.service';
 import {DeviceService} from "../../utils/device.service";
+import { DOCUMENT } from '@angular/common';
+import { PageScrollService } from 'ngx-page-scroll-core';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +13,20 @@ import {DeviceService} from "../../utils/device.service";
 export class HomeComponent implements OnInit {
 
 
-  constructor(public device: DeviceService,) {
+  constructor(public device: DeviceService,
+              @Inject(DOCUMENT) private document: any,
+              private pageScrollService: PageScrollService) {
   }
 
   ngOnInit(): void {
     // this.sheetService.addRow()
+    setTimeout(() => {
+      this.pageScrollService.scroll({
+        document: this.document,
+        scrollTarget: '#landing_home',
+        duration: 500,
+      });
+    }, 100)
   }
 
 
